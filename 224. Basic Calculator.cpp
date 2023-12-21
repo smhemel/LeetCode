93a9fb775abb2509d4af80972314b277508fd06c
S M HEMEL <smhemel.eu@gmail.com>
2023-12-21 21:43:33 +0600

    Create 224. Basic Calculator.cpp

---
+++ 224. Basic Calculator.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        long long int sum = 0;
+        int sign = 1;
+        stack<pair<int,int>> st;
+
+        for(int i=0; i<s.size();i++){
+            if(isdigit(s[i])){
+                long long int num = 0;
+                while(i<s.size() && isdigit(s[i])){
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                i--;
+                sum += num * sign;
+                sign = 1;
+            }
+            else if(s[i] == '('){
+                st.push({sum, sign});
+                sum = 0;
+                sign = 1;
+            }
+            else if(s[i] == ')'){
+                sum = st.top().first + (st.top().second * sum);
+                st.pop();
+
+            }
+            else if(s[i] == '-'){
+                sign = -1 * sign;
+            }
+        }
+        return sum;
+    }
+};
