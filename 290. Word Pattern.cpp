2976fd47ee9f67d5fc96c478abfab87c0995f2e1
S M HEMEL <smhemel.eu@gmail.com>
2023-12-23 19:35:19 +0600

    Create 290. Word Pattern.cpp

---
+++ 290. Word Pattern.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string>mp;
+        unordered_map<string,char>mp2;
+        int j = 0;
+        int i = 0;
+
+        while (i<pattern.length() && j<s.length()) {
+            string str = "";
+
+            while(j<s.length() && s[j]!=' '){
+                str+=s[j];
+                j++;
+            }
+            j++;
+
+            if(mp.count(pattern[i]))
+                if(mp[pattern[i]]!=str)
+                    return false;
+
+            if(mp2.count(str))
+                if(mp2[str]!=pattern[i])
+                return false;
+
+            mp[pattern[i]]=str;
+            mp2[str]=pattern[i];
+            i++;
+        }
+
+        if(j<s.length()) return false;
+        if(i<pattern.length()) return false;
+
+        return true;
+    }
+};
