e23baaa7a005c0ffb0e1045c78874cc68028f9e6
S M HEMEL <smhemel.eu@gmail.com>
2023-12-31 13:54:39 +0600

    Create 35. Search Insert Position.cpp

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        for (; i<nums.size(); i++)
+            if (nums[i] == target || nums[i] > target) return i;
+            
+        return i;
+    }
+};
