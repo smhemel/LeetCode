9cf5a24fa5af147ddbb24f5ff9d3fd564b2543e2
S M HEMEL <smhemel.eu@gmail.com>
2023-12-31 14:28:12 +0600

    Create 58. Length of Last Word.cpp

---
+++ 58. Length of Last Word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int cnt=0;
+
+        for(int i=s.size()-1; i>=0; i--) {
+            if(s[i] == ' ' && cnt > 0) return cnt;
+            else if (s[i] != ' ') cnt++;
+        }
+
+        return cnt;
+    }
+};
