85a18076fd3cf130bbdb1006ec44f4fd450e76c4
S M HEMEL <smhemel.eu@gmail.com>
2024-03-13 15:18:18 +0600

    Create 2485. Find the Pivot Integer.cpp

---
+++ 2485. Find the Pivot Integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum = n * (n + 1) / 2;
+        double a = sqrt(sum);
+
+        if (a - ceil(a) == 0) return int(a);
+        
+        return -1;
+    }
+};
