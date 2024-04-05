5b2204f51e760a0ff18d1bcd0c21df47a1f11237
S M HEMEL <smhemel.eu@gmail.com>
2024-04-06 03:18:41 +0600

    Create 1544. Make The String Great.cpp

---
+++ 1544. Make The String Great.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stack;
+        
+        for (char c : s) {
+            if (!stack.empty() && abs(c - stack.top()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        string result;
+        while (!stack.empty()) {
+            result = stack.top() + result;
+            stack.pop();
+        }
+        
+        return result;
+    }
+};
