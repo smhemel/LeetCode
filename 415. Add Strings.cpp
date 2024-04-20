7d53e216d4e1e5dc9e5f54eb1f7cb63a91d12b75
S M HEMEL <smhemel.eu@gmail.com>
2024-04-20 13:01:45 +0600

    Update 415. Add Strings.cpp

--- 415. Add Strings.cpp
+++ 415. Add Strings.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int carry=0,i=num1.length()-1,j=num2.length()-1;
-        string ans="";
-        while(i>=0||j>=0||carry>0){
-            if(i>=0){
-                carry=carry+num1[i]-'0';
-                i--;
+        string result;
+        int i = num1.size() - 1, j = num2.size() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1[i--] - '0';
             }
-            if(j>=0){
-                carry=carry+num2[j]-'0';
-                j--;
+            if (j >= 0) {
+                sum += num2[j--] - '0';
             }
-            
-             ans += char(carry % 10 + '0');
-            carry =carry/ 10;
+            carry = sum > 9 ? 1 : 0;
+            result += to_string(sum % 10);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        if (carry) result += to_string(carry);
+        reverse(result.begin(), result.end());
+        
+        return result;
     }
 };
