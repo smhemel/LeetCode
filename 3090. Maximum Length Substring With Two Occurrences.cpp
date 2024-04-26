457744c2e59f0ab14a4b18d4ab5e19ca1e78c05e
S M HEMEL <smhemel.eu@gmail.com>
2024-04-26 23:46:01 +0600

    Create 3090. Maximum Length Substring With Two Occurrences.cpp

---
+++ 3090. Maximum Length Substring With Two Occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        map<char,int>mp;
+        int i = 0, ans = 0;
+
+        for(int j = 0; j<s.size(); j++) {
+            mp[s[j]]++;
+
+            while(true) {
+                int maxF = 0;
+                for(auto it: mp) maxF = max(maxF, it.second);
+                if(maxF > 2) mp[s[i]]--, i++;
+                else break;
+            }
+
+            ans = max(ans, j-i+1);
+        }
+        
+        return ans;
+    }
+};
