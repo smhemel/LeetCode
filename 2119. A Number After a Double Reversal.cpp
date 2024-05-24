ae8fa4123b2c0605ccd8a80e20df0dd04ceefa8c
S M HEMEL <smhemel.eu@gmail.com>
2024-05-24 20:59:35 +0600

    Create 2119. A Number After a Double Reversal.cpp

---
+++ 2119. A Number After a Double Reversal.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        if (num == 0) return true;
+        else if (num % 10 == 0) return false;
+
+        return true;
+    }
+};
