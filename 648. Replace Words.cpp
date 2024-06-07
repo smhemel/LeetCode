a01d066074058dfea12247c154577a6a01680e29
S M HEMEL <smhemel.eu@gmail.com>
2024-06-07 21:35:03 +0600

    Create 648. Replace Words.cpp

---
+++ 648. Replace Words.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> rootSet(dictionary.begin(), dictionary.end());
+
+        istringstream iss(sentence);
+        string word, result;
+
+        while (iss >> word) {
+            string prefix;
+            for (int i = 1; i <= word.size(); ++i) {
+                prefix = word.substr(0, i);
+                if (rootSet.find(prefix) != rootSet.end()) break;
+            }
+
+            result += prefix + " ";
+        }
+
+        if (!result.empty()) result.pop_back();
+
+        return result;
+    }
+};
