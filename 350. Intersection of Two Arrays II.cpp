0d67aef26cd3bcb8520206e66de65fd2b7fbefe5
S M HEMEL <smhemel.eu@gmail.com>
2024-06-17 00:10:30 +0600

    Create 350. Intersection of Two Arrays II.cpp

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int, int> box;
+        for (int num : nums1) box[num]++;
+
+        for (int i = 0; i < nums2.size(); ++i) {
+            if (box[nums2[i]] > 0) {
+                res.push_back(nums2[i]);
+                box[nums2[i]]--;
+            }
+        }
+
+        return res;
+    }
+};
