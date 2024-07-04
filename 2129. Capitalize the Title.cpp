726be34c0d23536d99fa32f7eb3937f45ac77395
S M HEMEL <smhemel.eu@gmail.com>
2024-07-04 18:14:24 +0600

    Create 2129. Capitalize the Title.cpp

---
+++ 2129. Capitalize the Title.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        int len = title.length();
+
+        for (int i = 0; i < len; ++i) {
+            int firstIndex = i;
+
+            while (i < len && title[i] != ' ') {
+                title[i] = tolower(title[i]);
+                ++i;
+            }
+
+            if (i - firstIndex > 2) title[firstIndex] = toupper(title[firstIndex]);
+        }
+
+        return title;
+    }
+};
