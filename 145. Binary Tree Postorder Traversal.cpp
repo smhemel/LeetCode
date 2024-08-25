f779c11f0679d99fc6754fd8668188292cd42104
S M HEMEL <smhemel.eu@gmail.com>
2024-08-25 21:03:22 +0600

    Create 145. Binary Tree Postorder Traversal.cpp

---
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void find(TreeNode* root, vector<int>& ans) {
+        if (!root) return;
+        find(root->left, ans);
+        find(root->right, ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        find(root, ans);
+        return ans;
+    }
+};
