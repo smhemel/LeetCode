5df253926ebe59e8f0e831095a2ee41ef24f7d78
S M HEMEL <smhemel.eu@gmail.com>
2024-09-01 21:27:33 +0600

    Create 2022. Convert 1D Array Into 2D Array.cpp

---
+++ 2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        const int sz = original.size();
+        if (sz != m * n) return {};
+        vector<vector<int>> ans(m);
+        for (int i = 0; i < m; i++) ans[i].assign(original.begin() + i * n, original.begin() + (i + 1) * n);
+        return ans;
+    }
+};
