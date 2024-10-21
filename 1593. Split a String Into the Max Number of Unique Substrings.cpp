f025a2e5b1420377e89a1f9c495acff407612579
S M HEMEL <smhemel.eu@gmail.com>
2024-10-21 18:52:44 +0600

    Create 1593. Split a String Into the Max Number of Unique Substrings.cpp

---
+++ 1593. Split a String Into the Max Number of Unique Substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int backtrack(int start, const string& s, unordered_set<string>& seen) {
+        if (start == s.size()) {
+            return 0;
+        }
+        int maxSplits = 0;
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+
+            if (seen.find(substring) == seen.end()) {
+                seen.insert(substring);
+                maxSplits = max(maxSplits, 1 + backtrack(end, s, seen));
+                seen.erase(substring);
+            }
+        }
+        
+        return maxSplits;
+    }
+
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return backtrack(0, s, seen);
+    }
+};
