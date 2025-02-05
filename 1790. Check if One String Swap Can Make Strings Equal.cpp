8accec178125a4774a2732b47c5970fdadfbaa83
S M HEMEL <smhemel.eu@gmail.com>
2025-02-05 19:33:23 +0600

    Create 1790. Check if One String Swap Can Make Strings Equal.cpp

---
+++ 1790. Check if One String Swap Can Make Strings Equal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int x = -1;
+        
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
+                if (x == -1) x = i;
+                else {
+                    swap(s1[i], s1[x]);
+                    return s1 == s2;
+                }
+            }
+        }
+        return s1 == s2;
+    }
+};
