2f8feb7774dcb8310996966cc73711e4b00236dc
S M HEMEL <smhemel.eu@gmail.com>
2025-02-10 19:29:21 +0600

    Create 3174. Clear Digits.cpp

---
+++ 3174. Clear Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res = "";
+        
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!res.empty()) {
+                    res.pop_back();
+                }
+            } else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
